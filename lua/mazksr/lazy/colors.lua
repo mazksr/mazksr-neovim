79dd704db0f06a2861d906a059fa0a1e250bf2f8
mazksr <azkasufirman3@gmail.com>
2025-04-30 10:40:15 +0800

    remove readme

---
+++ lua/mazksr/lazy/colors.lua
@@ -0,0 +1,19 @@
+return {
+
+    {
+        "realbucksavage/riderdark.vim",
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
+
+}
